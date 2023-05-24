fc515d65f09b0d7b5f11ec2c7376c3d564b9a704
Rachel <rachel.szenberg@gmail.com>
2023-05-24 14:14:05 -0300

    update expense

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { ExpenseForm } from './ExpenseForm';
-import { editExpense, startRemoveExpense } from '../actions/expenses';
+import { startEditExpense, startRemoveExpense } from '../actions/expenses';
 import { useNavigate } from 'react-router-dom';
 
 const EditExpensePage = (props) => {
@@ -11,7 +11,7 @@ const EditExpensePage = (props) => {
             <ExpenseForm
                 expense={props.expense}
                 onSubmit={(expense) => {
-                    props.dispatch(editExpense(props.expense.id, expense));
+                    props.dispatch(startEditExpense(props.expense.id, expense));
                     navigate('/');
                 }}
             />
