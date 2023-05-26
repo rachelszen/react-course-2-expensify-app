cc349c2f1e466ff60bd6da0b620a0b3bd6bf6375
Rachel <rachel.szenberg@gmail.com>
2023-05-26 19:24:53 -0300

    styling list filters

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,7 +5,6 @@ import { getVisibleExpenses } from "../selectors/expenses";
 
 const ExpenseList = (props) => (
     <div>
-        <h1>Expense List</h1>
         {
             props.expenses.length === 0 ? (
                 <p>No expenses</p>
